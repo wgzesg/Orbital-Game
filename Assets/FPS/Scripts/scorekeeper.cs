6bfd0992dbf214d96c1b30121c119d3004b4a8f8
zesong <wgzesg@gmail.com>
2020-06-01 14:00:29 +0800

    added scoreboard in secondary scene2

--- Assets/FPS/Scripts/scorekeeper.cs
+++ Assets/FPS/Scripts/scorekeeper.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class scorekeeper : MonoBehaviour
 {
+    public int x_cood = 50;
+    public int y_cood= 50;
+    public int length = 100;
+    public int width = 75;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,7 @@ public class scorekeeper : MonoBehaviour
 
     private void OnGUI()
     {
-        GUI.Box(new Rect(100, 100, 100, 70), "Score");
+        GUI.Box(new Rect(x_cood, y_cood, length, width), "Score");
 
     }
 }
