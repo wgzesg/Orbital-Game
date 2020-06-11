c567b2d3ebe0afba4b07a3a4bdedf1bf6f5f8f9b
zesong <wgzesg@gmail.com>
2020-06-11 23:39:55 +0800

    primary version of menu with press of E

--- Assets/FPS/Scripts/PlayerCharacterController.cs
+++ Assets/FPS/Scripts/PlayerCharacterController.cs
@@ -242,26 +242,6 @@ public class PlayerCharacterController : MonoBehaviour
 
     void HandleCharacterMovement()
     {
-        /*
-        // horizontal character rotation
-        {
-            // rotate the transform with the input speed around its local Y axis
-            transform.Rotate(new Vector3(0f, m_InputHandler.GetLookInputsHorizontal() * rotationSpeed * RotationMultiplier
-                , 0f), Space.Self);
-        }
-
-        // vertical camera rotation
-        {
-            // add vertical inputs to the camera's vertical angle
-            m_CameraVerticalAngle += m_InputHandler.GetLookInputsVertical() * rotationSpeed * RotationMultiplier;
-
-            // limit the camera's vertical angle to min/max
-            m_CameraVerticalAngle = Mathf.Clamp(m_CameraVerticalAngle, -89f, 89f);
-
-            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
-            playerCamera.transform.localEulerAngles = new Vector3(m_CameraVerticalAngle, 0, 0);
-        }
-        */
 
         // character movement handling
         bool isSprinting = m_InputHandler.GetSprintInputHeld();
