c62d1af9814dc9a81a0c4623343cbd9cdc3eeb3d
lunzard <60387314+lunzard@users.noreply.github.com>
2020-06-22 17:06:23 +0800

    add tracing loots

---
+++ Assets/TeamS2S/LootFollowTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootFollowTarget : MonoBehaviour
+{
+    public Transform Target;
+    Vector3 _velocity = Vector3.zero;
+    public float MinModifier;
+    public float MaxModifier;
+    bool _isFollowing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartFollowing()
+    {
+        _isFollowing = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isFollowing)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, Time.deltaTime * Random.Range(MinModifier, MaxModifier));
+        }
+    }
+}
+
