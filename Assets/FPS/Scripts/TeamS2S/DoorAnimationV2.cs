1a94e44ceb26d3dc60270d749e2a2616d43bb520
lunzard <60387314+lunzard@users.noreply.github.com>
2020-06-25 14:57:12 +0800

    try to let enemy pass through door
    i create another test scene under the scene folder.
    The enemy with additional rigidbbody and boxcollider can trigger the door to open, but now player cannot shoot them down, Probably the box collider block the bullet.I will push another time later

---
+++ Assets/FPS/Scripts/TeamS2S/DoorAnimationV2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimationV2 : MonoBehaviour
+{
+    Animator door_animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door_animator = GetComponentInParent<Animator>();
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player" ||collision.tag == "Enemy")
+        {
+            Debug.Log("I am open now");
+            door_animator.SetBool("character_nearby", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Enemy")
+        {
+            door_animator.SetBool("character_nearby", false);
+        }
+    }
+}
