9cb728a62b0282c64866def3e2dac2f78026249c
lunzard <60387314+lunzard@users.noreply.github.com>
2020-07-08 22:47:44 +0800

    Multiplayer Mode: Create lobby and waiting room
    i link the lobby to a duplicate of secondary scene 2, which will be labled as level  5 in the build setting. You can change the load level in the multiplayersetting script from 5 back to 2(if i rmb correctly). Do check in the build setting.

---
+++ Assets/Multiplayer_S2S/Scripts_Multi/MultiplayerSetting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSetting : MonoBehaviour
+{
+    public static MultiplayerSetting multiplayerSetting;
+
+    public bool delayStart;
+    public int maxPlayers;
+
+    public int menuScene;
+    public int multiplayerScene;
+
+    private void Awake()
+    {
+        if (MultiplayerSetting.multiplayerSetting == null)
+        {
+            MultiplayerSetting.multiplayerSetting = this;
+        }
+        else
+        {
+            if (MultiplayerSetting.multiplayerSetting != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Multiplayer_S2S/Scripts_Multi/PhotonLobby.cs
@@ -0,0 +1,74 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby; // singletons
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this; // reference instance of this class
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); // connect to Master photon server.
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        PhotonNetwork.AutomaticallySyncScene = true; // change scene in the same way, same time
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        Debug.Log("BattleButtom was clicked");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create a new Room");
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)MultiplayerSetting.multiplayerSetting.maxPlayers };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("we are now in a room");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed. There must already be a room with the same name");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("CancelButton was clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
+
