fdc1d53c3c05a4bf8d29fe629a71600866269016
lunzard <60387314+lunzard@users.noreply.github.com>
2020-07-24 00:18:41 +0800

    update quit button in Multiplayer mode
    player can return to the menuScene when they press quit in the tab
    However, the reconnection is still a problem

---
+++ Assets/Multiplayer_S2S/Scripts_Multi/VariantScript/NetQuit.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetQuit : MonoBehaviour
+{
+    public void LeavePlayer()
+    {
+        StartCoroutine(LeaveAndLoad());
+    }
+
+    IEnumerator LeaveAndLoad()
+    {
+        Debug.Log("try to leave room...");
+        PhotonNetwork.LeaveRoom();
+        while (PhotonNetwork.InRoom)
+            yield return null;
+        Debug.Log("leaveroom finished, load to menuScene ...");
+        SceneManager.LoadScene(MultiplayerSettingV2.multiplayerSettingV2.menuScene);
+    }
+}
